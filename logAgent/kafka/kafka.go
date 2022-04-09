0c9ce0a1e31f6c647d54e0313ace824ab17c7be0
dqzboy <42825450+dqzboy@users.noreply.github.com>
2022-04-09 22:02:00 +0800

    Update kafka.go

--- logAgent/kafka/kafka.go
+++ logAgent/kafka/kafka.go
@@ -1,1 +1,32 @@
-kafka
+package kafka
+
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+)
+
+// 专门往kafka写日志的模块
+func main() {
+	config := sarama.NewConfig()
+	//tailf 包使用
+	config.Producer.RequiredAcks = sarama.WaitForAll //WaitForAll等待所有节点响应
+	config.Producer.Partitioner = sarama.NewHashPartitioner
+	config.Producer.Return.Successes = true
+	msg := &sarama.ProducerMessage{}
+	msg.Topic = "web_log"                                  //topic名称
+	msg.Value = sarama.StringEncoder("this ia a test log") //使用sarama.StringEncoder序列化
+	//连接kafka
+	clinet, err := sarama.NewSyncProducer([]string{"192.168.66.10:9092"}, config)
+	if err != nil {
+		fmt.Println("producer closed, err:", err)
+		return
+	}
+	defer clinet.Close()
+	//发送消息
+	pid, offset, err := clinet.SendMessage(msg)
+	if err != nil {
+		fmt.Println("sed msg failed, err:", err)
+		return
+	}
+	fmt.Printf("pid:%v offset:%v\n", pid, offset)
+}
