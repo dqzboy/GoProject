ac3fc8e77f53361156dbda0d615e89a94fd21b62
dqzboy <42825450+dqzboy@users.noreply.github.com>
2022-04-09 23:04:38 +0800

    Update kafka.go

--- logAgent/kafka/kafka.go
+++ logAgent/kafka/kafka.go
@@ -6,24 +6,36 @@ import (
 )
 
 // 专门往kafka写日志的模块
-func main() {
+var (
+	client sarama.SyncProducer //声明一个全局的连接kafka的生产者
+
+)
+
+// Init 初始化
+func Init(addrs []string) (err error) {
 	config := sarama.NewConfig()
 	//tailf 包使用
-	config.Producer.RequiredAcks = sarama.WaitForAll //WaitForAll等待所有节点响应
-	config.Producer.Partitioner = sarama.NewHashPartitioner
-	config.Producer.Return.Successes = true
-	msg := &sarama.ProducerMessage{}
-	msg.Topic = "web_log"                                  //topic名称
-	msg.Value = sarama.StringEncoder("this ia a test log") //使用sarama.StringEncoder序列化
+	config.Producer.RequiredAcks = sarama.WaitForAll        //WaitForAll等待所有节点响应
+	config.Producer.Partitioner = sarama.NewHashPartitioner //新选出一个partition
+	config.Producer.Return.Successes = true                 //成功发送的消息将在succcess channel返回
+
 	//连接kafka
-	clinet, err := sarama.NewSyncProducer([]string{"192.168.66.10:9092"}, config)
+	client, err = sarama.NewSyncProducer(addrs, config)
 	if err != nil {
 		fmt.Println("producer closed, err:", err)
 		return
 	}
-	defer clinet.Close()
-	//发送消息
-	pid, offset, err := clinet.SendMessage(msg)
+	return
+}
+
+func SendToKafka(topic, data string) {
+	//构造一个消息
+	msg := &sarama.ProducerMessage{}
+	msg.Topic = topic                      //topic名称
+	msg.Value = sarama.StringEncoder(data) //使用sarama.StringEncoder序列化
+
+	//发送到kafka
+	pid, offset, err := client.SendMessage(msg)
 	if err != nil {
 		fmt.Println("sed msg failed, err:", err)
 		return
