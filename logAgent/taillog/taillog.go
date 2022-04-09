7c702107413d5a25364ba482da5b8c208a68c3e2
dqzboy <42825450+dqzboy@users.noreply.github.com>
2022-04-09 23:05:01 +0800

    Update taillog.go

--- logAgent/taillog/taillog.go
+++ logAgent/taillog/taillog.go
@@ -3,12 +3,15 @@ package taillog
 import (
 	"fmt"
 	"github.com/hpcloud/tail"
-	"time"
 )
 
-//专门从日志文件收集日志的模块
-func main() {
-	fileName := "./my.log" //日志文件路径和日志名称
+var (
+	tailObj *tail.Tail
+	LogChan chan string
+)
+
+// Init 专门从日志文件收集日志的模块
+func Init(fileName string) (err error) {
 	config := tail.Config{
 		ReOpen:    true,                                 //重新打开，日志切割
 		Follow:    true,                                 //是否跟随上次关闭的文件
@@ -16,22 +19,14 @@ func main() {
 		MustExist: false,                                //文件不存在不报错
 		Poll:      true,
 	}
-	tails, err := tail.TailFile(fileName, config)
+	tailObj, err = tail.TailFile(fileName, config)
 	if err != nil {
 		fmt.Println("tail file failed, err:", err)
 		return
 	}
-	var (
-		line *tail.Line
-		ok   bool
-	)
-	for {
-		line, ok = <-tails.Lines
-		if !ok {
-			fmt.Printf("tail file close reopen, filename: %s\n", tails.Filename)
-			time.Sleep(time.Second)
-			continue
-		}
-		fmt.Println("line:", line.Text)
-	}
+	return
+}
+
+func ReadChan() <-chan *tail.Line {
+	return tailObj.Lines
 }
